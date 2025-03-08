8126d37d69df21289ad9ca6eb9f6a5bcb31c00f8
nerma <n.ermak08@yandex.by>
2025-03-08 22:47:51 +0300

    added basic config

---
+++ config.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel, Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class V1Config(BaseModel):
+    prefix: str = Field(default='/v1')
+
+class APIConfig(BaseModel):
+    prefix: str = Field(default='/api')
+
+    v1: V1Config = V1Config()
+
+class DocsConfig(BaseModel):
+    prefix: str = Field(default='/docs')
+
+class RedocConfig(BaseModel):
+    prefix: str = Field(default='/redoc')
+
+class SwaggerConfig(BaseModel):
+    docs: DocsConfig = DocsConfig()
+    redoc: RedocConfig = RedocConfig()
+
+class ApplicationConfig(BaseModel):
+    debug: bool = Field(default=False)
+    title: str = Field(default='gomeltrans-api')
+    description: str = Field(default='Unofficial API')
+
+    swagger: SwaggerConfig = SwaggerConfig()
+    api: APIConfig = APIConfig()
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        case_sensitive=False,
+        env_nested_delimiter='__',
+        env_prefix='CONFIG__',
+    )
+
+    app: ApplicationConfig = ApplicationConfig()
+
+settings: Settings = Settings()
