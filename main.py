55122f7fbc43fcc2bea789d9eb7bcd74637b4250
nerma <n.ermak08@yandex.by>
2025-03-09 23:44:44 +0300

    added method get_application

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ from fastapi import FastAPI
 from config import settings
 from src import router as main_router
 from fastapi.staticfiles import StaticFiles
+from src.exceptions.route_not_found import route_not_found_exception_handler
+from src.controllers.gomeltrans_client.exceptions import RouteNotFoundException
 
 
-app: FastAPI = FastAPI(
-    debug=settings.app.debug,
-    title=settings.app.title,
-    docs_url=settings.app.swagger.docs.prefix if settings.app.debug else None,
-    redoc_url=settings.app.swagger.redoc.prefix if settings.app.debug else None,
-)
+def get_application() -> FastAPI:
+    app: FastAPI = FastAPI(
+        debug=settings.app.debug,
+        title=settings.app.title,
+        docs_url=settings.app.swagger.docs.prefix if settings.app.debug else None,
+        redoc_url=settings.app.swagger.redoc.prefix if settings.app.debug else None,
+    )
 
+    app.add_exception_handler(RouteNotFoundException, route_not_found_exception_handler)
 
-app.mount('/src/static', StaticFiles(directory='src/static'), name='static')
-app.include_router(main_router)
+    app.mount('/src/static', StaticFiles(directory='src/static'), name='static')
+    app.include_router(main_router)
+    return app
+
+app: FastAPI = get_application()
