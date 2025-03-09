10848c794507dcef044ba10afe467c86369a0aee
nerma <n.ermak08@yandex.by>
2025-03-09 17:24:47 +0300

    extra comment removed

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,5 @@
 from fastapi import APIRouter
 from .api import router as api_router
-# from .swagger import router as swagger_router
-
 
 router: APIRouter = APIRouter()
 router.include_router(api_router)
