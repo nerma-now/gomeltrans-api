ec616fdc3efd4c8be6739f4d697de7c481d6f8de
nerma <n.ermak08@yandex.by>
2025-03-10 23:03:21 +0300

    added new method for getting routes

--- src/controllers/gomeltrans_client/client.py
+++ src/controllers/gomeltrans_client/client.py
@@ -1,12 +1,14 @@
-from async_lru import alru_cache
+from typing import Union
+from config import settings
 from http import HTTPStatus
 from pydantic import HttpUrl
-from config import settings
+from async_lru import alru_cache
 from .enums import TypeTransport
-from bs4 import BeautifulSoup, ResultSet
+from src.schemas.route import Route, Routes
+from .exceptions import RouteNotFoundException
 from src.schemas.stop import RouteStop, RouteStops
 from src.controllers.http_client.client import HTTPClient
-from .exceptions import RouteNotFoundException
+from bs4 import BeautifulSoup, ResultSet, SoupStrainer, Tag
 
 
 class GomelTransClient(HTTPClient):
@@ -16,15 +18,71 @@ class GomelTransClient(HTTPClient):
 
         self._base_url = base_url
 
-    @alru_cache(maxsize=32)
+    @alru_cache
+    async def get_routes(self,
+                         type_transport: TypeTransport):
+        url: str = self._base_url.__str__() + \
+            'routes/{type_transport}'.format(
+                type_transport=type_transport
+            )
+
+        async with HTTPClient() as http_client:
+            response, status = await http_client.get(
+                url=url
+            )
+
+            strainer: SoupStrainer = SoupStrainer(
+                name='td',
+                class_='content'
+            )
+            soup: BeautifulSoup = BeautifulSoup(
+                markup=response,
+                features='lxml',
+                parse_only=strainer
+            )
+
+            routes_list: ResultSet = soup.find_all(
+                name='div'
+            )
+
+            routes: list[Route] = list()
+
+            for route in routes_list:
+                b_element: Tag = route.find('b')
+                number: str = b_element.text
+
+                i_element: Union[Tag | str] = str()
+                days: str = str()
+                if i_element:
+                    i_element = route.find('i')
+                    days = i_element.text
+
+                a_element: Tag = route.find('a')
+                name: str = a_element.text \
+                    .replace(number, str()).replace(days, str()).replace('—', '→')
+                href: str = a_element.get('href')
+
+                routes.append(
+                    Route(
+                        number=number,
+                        name=name,
+                        href=href,
+                    )
+                )
+
+            return Routes(
+                routes=routes
+            )
+
+    @alru_cache
     async def get_route_stops(self,
                               type_transport: TypeTransport,
                               number: str) -> RouteStops:
         url: str = self._base_url.__str__() + \
             'routes/{type_transport}/{number}'.format(
-            type_transport=type_transport,
-            number=number
-        )
+                type_transport=type_transport,
+                number=number
+            )
 
         async with HTTPClient() as http_client:
             response, status = await http_client.get(
@@ -33,12 +91,18 @@ class GomelTransClient(HTTPClient):
 
             if status == HTTPStatus.NOT_FOUND:
                 raise RouteNotFoundException(
-                    f'Route not found'
+                    'Route not found'
                 )
 
+        strainer: SoupStrainer = SoupStrainer(
+            name='td',
+            class_='content'
+        )
         soup: BeautifulSoup = BeautifulSoup(
             markup=response,
-            features='lxml')
+            features='lxml',
+            parse_only=strainer
+        )
 
         td_tags: list[str] = ['t-left', 't-right']
         td_stops: list[ResultSet] = list()
