3dfc2153af9e62a2006ad1e0f624dc9a95a5ce9a
nerma <n.ermak08@yandex.by>
2025-03-10 23:00:58 +0300

    included new router

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from config import settings
 from .get_route_stops import router as get_route_stops_router
+from .get_routes import router as get_routes_router
 
 
 router: APIRouter = APIRouter(
     prefix=settings.app.api.v1.prefix,
 )
 router.include_router(get_route_stops_router)
+router.include_router(get_routes_router)
