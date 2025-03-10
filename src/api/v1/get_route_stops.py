5b2da54cd6a1d8e62c76438baf2a3af55712c39c
nerma <n.ermak08@yandex.by>
2025-03-10 23:01:49 +0300

    added answer to documentation

--- src/api/v1/get_route_stops.py
+++ src/api/v1/get_route_stops.py
@@ -1,9 +1,10 @@
 from http import HTTPStatus
 from fastapi import APIRouter
-from src.controllers.gomeltrans_client.client import gomeltrans_client
-from src.schemas.responses import Message
 from src.schemas.stop import RouteStops
+from src.schemas.responses import Message
 from src.controllers.gomeltrans_client.enums import TypeTransport
+from src.controllers.gomeltrans_client.client import gomeltrans_client
+
 
 router: APIRouter = APIRouter()
 
@@ -11,17 +12,21 @@ router: APIRouter = APIRouter()
     path='/get_route_stops',
     response_model=RouteStops,
     responses={
-        HTTPStatus.NOT_FOUND.value: {"model": Message},
+        HTTPStatus.NOT_FOUND.value:
+            {
+                'model': Message,
+                'description': 'Route not found'
+            },
     }
 )
 async def get_route_stops_handler(
         type_transport: TypeTransport,
         number: str,
 ) -> RouteStops:
-
     route_stops: RouteStops = await gomeltrans_client.get_route_stops(
         type_transport=type_transport,
         number=number
     )
+
     return route_stops
 
